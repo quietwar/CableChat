8c6658192b3ef78750ab4dc548075f29648d5f7a
Prof. Hodari Touré <hodari@hiddengeniusproject.org>
2017-04-22 14:15:41 -0700

    hello

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,8 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  config.action_cable.allowed_request_origins = ['https://dashboard.heroku.com/apps/cablechatroom/deploy/github',
+                                                 'https://dashboard.heroku.com/apps/cablechatroom/deploy/github']
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "CableChat_#{Rails.env}"
