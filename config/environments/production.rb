1dc15074445e9b621f4674b6df073c52e2c6ccdd
Prof. Hodari Touré <hodari@hiddengeniusproject.org>
2017-04-23 11:28:12 -0700

    fix envit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+
 
 config.action_cable.allowed_request_origins = ['https://sitepoint-actioncable.herokuapp.com',
                                                'http://sitepoint-actioncable.herokuapp.com']
